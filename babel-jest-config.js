a57d9ac01ba1117aaff46720ca029a3081a24597
Tony Anziano <toanzian@microsoft.com>
2019-02-26 12:38:20 -0800

    Got coverage working with all packages except playground.

--- babel-jest-config.js
+++ babel-jest-config.js
@@ -8,6 +8,9 @@ const transformer = require('babel-jest');
 const { createTransformer } = transformer;
 const thisTransformer = createTransformer(babelOptions);
 
+// Jest is supposed to use babel-jest to consume the .babelrc file in the root of the project,
+// but for some reason it can't seem to locate the file, so we must manually load the .babelrc
+// file in memory and create a transformer for it.
 Object.assign(transformer, thisTransformer);
 transformer.createTransformer = () => {
   return thisTransformer
--- packages/component/src/Blah.spec.js
+++
@@ -1,7 +0,0 @@
-import inputtableKey from './Utils/TypeFocusSink/inputtableKey';
-
-describe('some tests', () => {
-  it('input key', () => {
-    expect(inputtableKey('Add')).toBe('+');
-  });
-});
